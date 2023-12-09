2be36fa4928f592bd8dd424aadf2d0aeecd46c5e
AndrefHub <andrew02541632@gmail.com>
2023-12-09 13:32:03 +0300

    lab9 done

---
+++ lab7-10/src/TaskService.js
@@ -0,0 +1,31 @@
+import { Status } from "./const.js";
+import { tasks } from "./mock/task.js";
+
+export class TasksService {
+    #boardTasks = tasks;
+
+    getTasksByStatus(status) {
+        const tasksStatus = this.#boardTasks.filter(function (task) {
+            return task.status == status;
+        });
+        return (tasksStatus)
+    }
+
+    create({ id = '', title, status = Status.BACKLOG }) {
+        id = this.generateId();
+        this.#boardTasks.push({ id, title, status });
+        window.dispatchEvent(new CustomEvent("create-task"));
+    }
+
+    deleteAll() {
+        this.#boardTasks.length = 0;
+    }
+
+    getTasks() {
+        return this.#boardTasks;
+    }
+
+    generateId() {
+        return crypto.randomUUID();
+    }
+}
---
+++ lab7-10/src/mock/task.js
@@ -0,0 +1,47 @@
+export const tasks = [
+    {
+        'id': `1`,
+        'title': `Написать курсач`,
+        'status': `backlog`,
+    },
+    {
+        'id': `2`,
+        'title': `Сделать зелененькие кнопочки но чтобы они отливали синеньким`,
+        'status': `backlog`,
+    },
+    {
+        'id': `3`,
+        'title': `Добавить GUI`,
+        'status': `inProcess`,
+    },
+    {
+        'id': `4`,
+        'title': `Завершить обработку данных`,
+        'status': `inProcess`,
+    },
+    {
+        'id': `5`,
+        'title': `Добавить новый фунционал`,
+        'status': `done`,
+    },
+    {
+        'id': `6`,
+        'title': `Поспать`,
+        'status': `bin`,
+    },
+    {
+        'id': `7`,
+        'title': `Поспать`,
+        'status': `bin`,
+    },
+    {
+        'id': `8`,
+        'title': `Отоспаться`,
+        'status': `bin`,
+    },
+    {
+        'id': `9`,
+        'title': `uuh`,
+        'status': `bin`,
+    }
+]
