5323dd8fb7b3c71298da8ccfd284895a186dcebe
AndrefHub <andrew02541632@gmail.com>
2023-12-09 13:32:34 +0300

    lab10 done

---
+++ lab7-10/src/components/task-main-con.js
@@ -0,0 +1,16 @@
+import { createElement } from '../render.js';
+import { AbstractComponent } from './AbstractComponent.js';
+
+
+function createTaskMainCon() {
+  return (
+    `<section class="taskCardCon">     </section>`
+  );
+}
+
+
+export class TaskMainCon extends AbstractComponent {
+  getTemplate() {
+    return createTaskMainCon();
+  }
+}
